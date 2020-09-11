121276ad8cacf0a54b3528aacd55bdf1eb9f98c8
Matt McCormick <matt.mccormick@kitware.com>
2020-09-10 21:13:07 -0400

    Add plot.ly jupyterlab extensions

--- coiled/create_software_environment.py
+++ coiled/create_software_environment.py
@@ -7,6 +7,6 @@ import argparse
 coiled.create_software_environment(
     name="sc20-pyhpc",
     conda="environment.yml",
-    post_build=["jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib jupyterlab-datawidgets itkwidgets@0.32.0 dask-labextension",
+    post_build=["jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib jupyterlab-datawidgets itkwidgets@0.32.0 dask-labextension jupyterlab-plotly@4.10.0 plotlywidget@4.10.0",
                 "jupyter serverextension enable dask_labextension"],
 )
